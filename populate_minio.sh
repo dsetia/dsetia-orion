85a87bca648f2cf8ba78e175fc4144296ed9a789
Deepinder Setia <dsetia@yahoo.com>
2025-04-30 15:10:04 +0000

    Temporarily allow anonlymous access to minio with referrer header

--- populate_minio.sh
+++ populate_minio.sh
@@ -30,3 +30,26 @@ echo '{
 # Apply policy
 mc admin policy create myminio apiuser-policy apiuser_policy.json
 mc admin policy attach myminio apiuser-policy --user apiuser
+
+# Allow anonymous access for authenticated requests
+# allows s3:GetObject only for requests with a Referer header from the API server,
+
+echo '{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject"],
+      "Resource": ["arn:aws:s3:::software/*", "arn:aws:s3:::rules/*", "arn:aws:s3:::threatintel/*"],
+      "Condition": {
+        "StringEquals": {
+          "aws:Referer": "http://apis-container:8080"
+        }
+      }
+    }
+  ]
+}' > bucket_policy.json
+mc anonymous set-json bucket_policy.json myminio/software
+mc anonymous set-json bucket_policy.json myminio/rules
+mc anonymous set-json bucket_policy.json myminio/threatintel
