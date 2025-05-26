ddc59e6b7abf1586b9b1a9d18a007269b23f450e
Deepinder Setia <dsetia@yahoo.com>
2025-05-26 14:26:02 +0000

    Add code license

--- apis/apis.go
+++ apis/apis.go
@@ -1,3 +1,16 @@
+// Copyright (c) 2025 SecurITe
+// All rights reserved.
+//
+// This source code is the property of SecurITe.
+// Unauthorized copying, modification, or distribution of this file,
+// via any medium is strictly prohibited unless explicitly authorized
+// by SecurITe.
+//
+// This software is proprietary and confidential.
+//
+// File Owner:       deepinder@securite.world
+// Created On:       05/26/2025
+
 package main
 
 import (
